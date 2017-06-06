8bba02bf346e99b5614cfa936fd99db65a288b58
Danilo Paez en Mac <danilo@epy.me>
2017-06-06 19:06:55 -0300

    Agregamos eventos de ganar y empatar

--- client/equipos/equipos.js
+++ client/equipos/equipos.js
@@ -1,5 +1,14 @@
 Template.Equipo_tmp.helpers({
   equipos(){
-    return Equipos.find();
+    return Equipos.find({}, { sort : { puntos: -1 } });
+  }
+})
+
+Template.Equipo_tmp.events({
+  'click .gano'(){
+    Equipos.update(this._id, { $inc : { puntos: +3} } );
+  },
+  'click .empato'(){
+    Equipos.update(this._id, { $inc : { puntos: +1} } );
   }
 })
