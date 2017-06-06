1f222ec211991d67eb57f4a91bc9537367a0d96b
Danilo Paez en Mac <danilo@epy.me>
2017-06-06 18:41:50 -0300

    Agregamos Coleccion Equipos para mostrar Isomorfismo

--- client/equipos/equipos.js
+++ client/equipos/equipos.js
@@ -1,12 +1,5 @@
-datosDeEquipos = [
-  { club: 'Boca'},
-  { club: 'River'},
-  { club: 'Talleres'},
-  { club: 'Belgrano'},
-]
-
 Template.Equipo_tmp.helpers({
   equipos(){
-    return datosDeEquipos;
+    return Equipos.find();
   }
 })
